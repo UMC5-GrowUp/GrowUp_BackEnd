1fa34bd5d7db862b185d1a39b2112ac3eccff976
m._.jae <wlsaudwo295@naver.com>
2024-02-02 21:34:58 +0900

    paging추가

--- src/main/java/Growup/spring/participate/converter/ParticipateConverter.java
+++ src/main/java/Growup/spring/participate/converter/ParticipateConverter.java
@@ -15,25 +15,29 @@ import java.util.stream.Collectors;
 @RequiredArgsConstructor
 public class ParticipateConverter {
 
+
+
     public static ParticipateDtoRes.participateRes participateDto (Participate participate){
         return ParticipateDtoRes.participateRes.builder()
                 .nickName(participate.getUser().getNickName()) // user를 가져와 다시 nickname을 가져옴
                 .liked(participate.getLiked())
-                .is50Up(participate.getIs_50up())
-                .isBestUp(participate.getIs_bestup())
-                .participateTime(participate.getParticipateTime())
+                .photoUrl(participate.getUser().getPhotoUrl())
                 .build();
     }
 
     public static ParticipateDtoRes.orderByAsc orderByAscDto (List<Participate> participateList){
+
         // 다른 converter를 가져오는 법
         List<ParticipateDtoRes.participateRes> participateResList = participateList.stream()
                 .map(ParticipateConverter::participateDto)
                 .collect(Collectors.toList());
+
         //다른 converter 참조해서 참여자 리스트를 가져옴
         return ParticipateDtoRes.orderByAsc.builder()
                 .participateResList(participateResList)
                 .build();
     }
+
+
 
 }
--- src/main/java/Growup/spring/participate/repository/ParticipateRepository.java
+++ src/main/java/Growup/spring/participate/repository/ParticipateRepository.java
@@ -1,18 +1,19 @@
 package Growup.spring.participate.repository;
 
 import Growup.spring.participate.model.Participate;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.List;
-
 @Transactional
 public interface ParticipateRepository extends JpaRepository<Participate, Long> {
-    List<Participate> findAllByGrowRoomIdOrderByUser_nickNameAsc(Long growRoomId);
-    List<Participate> findAllByGrowRoomIdOrderByCreatedAtDesc (Long growRoomId);
-    List<Participate> findAllByGrowRoomIdOrderByLikedAsc(Long growRoomId);
+    Page<Participate> findAllByGrowRoomIdOrderByUser_nickNameAsc(Long growRoomId , PageRequest pageRequest);
+    Page<Participate> findAllByGrowRoomIdOrderByCreatedAtDesc (Long growRoomId , PageRequest pageRequest);
+    Page<Participate> findAllByGrowRoomIdOrderByLikedAsc(Long growRoomId , PageRequest pageRequest);
+    Page<Participate> findAllByGrowRoomIdOrderByParticipateTimeAsc(Long growRoomId , PageRequest pageRequest);
+    boolean existsById(Long growRoomId);
 
-    List<Participate> findAllByGrowRoomIdOrderByParticipateTimeAsc(Long growRoomId);
-    List findAllByGrowRoomId(Long growRoomId);
+
 
 }
