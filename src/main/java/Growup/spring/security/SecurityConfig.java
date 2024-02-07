8e911eaf6c61f0e32befe093822dcd87c9f00eeb
jungjoo <wjddis000507@gmail.com>
2024-02-07 17:07:36 +0900

    배포 확인

--- src/main/java/Growup/spring/security/SecurityConfig.java
+++ src/main/java/Growup/spring/security/SecurityConfig.java
@@ -47,7 +47,6 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
             return config;
         };
     }
-
     @Override
     protected void configure(HttpSecurity http) throws Exception {
 
