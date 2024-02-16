0da6360d9fc96a2c9df8f016176ef4c0c8822158
jungjoo <wjddis000507@gmail.com>
2024-02-16 11:06:34 +0900

    캘린더,투두리스트 작성자 작성/ ErrorStatus 수정

--- src/main/java/Growup/spring/todoList/controller/TodoListController.java
+++ src/main/java/Growup/spring/todoList/controller/TodoListController.java
@@ -25,7 +25,10 @@ public class TodoListController {
 
     private final TodoListService todoListService;
     private final JwtProvider jwtProvider;
-
+    /**
+     * 24.01.29 작성자 : 정주현
+     * 투두리스트 작성
+     */
     @ResponseBody
     @PostMapping("/enroll")
     public ApiResponse<TodoDtoListRes.todoEnrollRes> todoListEnroll(@RequestBody @Valid TodoDtoListReq.todoEnrollReq request){
@@ -33,19 +36,28 @@ public class TodoListController {
         TodoList todoList = todoListService.todoListEnroll(request,userId);
         return ApiResponse.onSuccess(TodoListConverter.todoListDtoRes(todoList));
     }
-
+    /**
+     * 24.01.29 작성자 : 정주현
+     * 투두리스트 조회
+     */
     @GetMapping("/inquiry")
     public ApiResponse<TodoDtoListRes.todoResultInquiryRes> todoListInquiry(){
         Long userId = jwtProvider.getUserID();
         return ApiResponse.onSuccess(TodoListConverter.todoResultInquiryRes(userId,todoListService.todoListInquiry(userId)));
     }
-
+    /**
+     * 24.01.29 작성자 : 정주현
+     * 투두리스트 내용 수정
+     */
     @PatchMapping("/modify-comment")
     public ApiResponse<SuccessStatus> todoListCommentModify(@RequestParam Long todoListId,@RequestBody TodoDtoListReq.todoCommentModifyReq request){
         todoListService.todoListCommentModify(todoListId,request);
         return ApiResponse.onSuccessWithoutResult(SuccessStatus._OK);
     }
-
+    /**
+     * 24.01.29 작성자 : 정주현
+     * 투두리스트 체크(상태) 수정
+     */
     @PatchMapping("/modify-status")
     public ApiResponse<SuccessStatus> modifyStatus(@RequestParam Long todoListId){
         todoListService.modifyservice(todoListId);
